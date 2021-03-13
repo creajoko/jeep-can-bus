fa6bb1e8f6727935c7aff56b13de45139d35aa1e
creajoko <joakim_korling@hotmail.com>
2021-03-13 09:38:43 +0100

    Working filter an profile msg

--- project/arduino/jeep-can-bus/jeep-can-bus-messages.h
+++ project/arduino/jeep-can-bus/jeep-can-bus-messages.h
@@ -2,21 +2,23 @@
 #define CAN_POWER 0x000
 #define CAN_BLINKERS 0x006
 #define CAN_RADIO_MODE 0x09f
-#define CAN_RADIO_CONTROLS 0x394 // Emitted by the radio when pressing buttons
+#define CAN_RADIO_CONTROLS 0x394 // Emitted by the radio
 #define CAN_VES_UNIT 0x3dd
 #define CAN_MULTI_SWITCH   0x11d
 #define CAN_HEADLIGHTS
 #define CAN_RADIO_SOUND_PROFILE 0x3D0
 
 // Pressing steeringwheel radio ctrls
-// 0x15 - radio on/off?
+// 0x15
 // 0x1B6
 // 0x3A0
-// 0x3D0 - from radio, ifo about radio/cd etc, what frequency, track etc?
+// 0x3D0 - sound profile Can Id, balance, fade, bass, mid, treble, 0ff, 
+//   1f-klar/ff-under ändring??, 7
+// 1- min/left/front/A-mitt/13-max/right/back
 // 0x3F8
 // 0x9F - from radio!
 
-// Ids added when Jeep stereo is connected vs disconnected
+// Ids added when Jeep stereo is connected (off)
 // 0x1AB
 // 0x416
 // 0x394
@@ -26,5 +28,5 @@
 
 // TBD
 // 0x2C0
-// 0x3F1 - radio on / off state or button press?
+// 0x3F1 - radio on / off state?
 // 0x159
--- project/arduino/jeep-can-bus/jeep-can-bus.ino
+++ project/arduino/jeep-can-bus/jeep-can-bus.ino
@@ -4,7 +4,24 @@
 #include "mcp_can.h"
 #include "jeep-can-bus-messages.h"
 
-// Operational control of the scetch
+
+#define CAN_MODULE_CS_PIN 9
+#define CHECK_PERIOD_MS 200
+#define ANNOUNCE_PERIOD_MS 500
+#define BUTTON_PRESS_DEBOUNCE_MS 350
+#define CAN_DELAY_AFTER_SEND 20
+#define DISPLAY_REFRESH_PERIOD 500
+#define MESSAGE_LEN 8
+#define FIRST_ANNOUNCE_AFTER 10000
+
+MCP_CAN CAN(CAN_MODULE_CS_PIN);
+
+unsigned long lastCheck = 0;
+unsigned long lastAnnounce = millis();
+unsigned long lastButtonPress = 0;
+unsigned long lastDisplayRefresh = 0;
+
+// Operational control
 #define RADIOMODE_OTHER 0
 #define RADIOMODE_AUX 1
 //#define SCANNER
@@ -13,27 +30,17 @@
 // Filtersetting max 20
 //int can_id_filter[] = {0x015, 0x2C0, 0x3D0, 0x3F1, CAN_RADIO_MODE, 0x159, 0x1B6};
 //unsigned int can_id_filter[] = {0x015};
-int can_id_filter[] = {0x3F1, CAN_RADIO_MODE, CAN_RADIO_SOUND_PROFILE};
+int can_id_filter[] = { 0x3F1, CAN_RADIO_MODE, CAN_RADIO_SOUND_PROFILE, 0x3D0};
+int filter_len = sizeof(can_id_filter);
 
-#define CAN_MODULE_CS_PIN 9
-#define CHECK_PERIOD_MS 200
-#define ANNOUNCE_PERIOD_MS 500
-#define BUTTON_PRESS_DEBOUNCE_MS 350
-#define CAN_DELAY_AFTER_SEND 20
-#define DISPLAY_REFRESH_PERIOD 500
-
-MCP_CAN CAN(CAN_MODULE_CS_PIN);
-
-unsigned long lastCheck = 0;
-unsigned long lastAnnounce = 0;
-unsigned long lastButtonPress = 0;
-unsigned long lastDisplayRefresh = 0;
-
-// Outgoing Messages
+// Messages
 #define msgVesAuxModeLen 8
 unsigned char msgVesAuxMode[8] = {3, 0, 0, 0, 0, 0, 0, 0};
 #define msgPowerOnLen 6 // Emmited by car when power is on
 unsigned char msgPowerOn[6] = {0x63, 0, 0, 0, 0, 0};
+
+#define msgProfile1Len 7 // Emmited by car when power is on
+unsigned char msgProfile1[7] = {CAN_RADIO_SOUND_PROFILE, 1, 1, 1, 1, 1, 0xFF};
 
 unsigned char radioMode = RADIOMODE_OTHER;
 const char compileDate[] = __DATE__ " " __TIME__;
@@ -52,42 +59,29 @@ void setup() {
 }
 
 int display[20][20];
-
 void clearScr() { // Works w putty terminal
   Serial.write(27);
   Serial.print("[2J");
   Serial.write(27);
   Serial.print("[H");
-  }
+}
   
 void displayScr() {
   clearScr();
   for(int i=0;i<10;i++) {
-    for(int j=0;j<20;j++) {
+    for(int j=0;j<10;j++) {
       char cell[4];
       sprintf(cell, " %03X",display[i][j]);
       Serial.print(cell);
+      }
+      Serial.println();
     }
-    Serial.println();
+    lastDisplayRefresh = millis();
   }
-  lastDisplayRefresh = millis();
-}
-
-bool inArray(int val, int arr[]) {
-  for (int i = 0; i < sizeof(arr); i++) {
-    if (val == arr[i]) {
-      return i;
-    }
-  }
-  return 99;
-}
-
+ 
 void sendAnnouncements() {
-#ifdef BENCH_MODE_ON
-  // when on bench - send power on command to radio to enable it
-  CAN.sendMsgBuf(CAN_POWER, 0, msgPowerOnLen, msgPowerOn);
+  CAN.sendMsgBuf(CAN_RADIO_SOUND_PROFILE, 0, msgProfile1Len, msgProfile1);
   delay(CAN_DELAY_AFTER_SEND);
-#endif
 }
 
 unsigned int canId = 0;
@@ -95,14 +89,24 @@ unsigned char len = 0;
 unsigned char buf[8];
 unsigned char newMode = 0;
 
+int inArray(int val, int arr[]) {
+  for (int i = 0; i < filter_len; i++) {
+    if (val == arr[i]) {
+      return i;
+    }
+  }
+  return 99;
+}
+
+
+
 void checkIncomingMessages() {
   if (CAN_MSGAVAIL != CAN.checkReceive())
     return;
   CAN.readMsgBuf(&len, buf);
   canId = CAN.getCanId();
-
 #ifdef SCANNER
-  // All messages on the c-bus as csv
+  // All messages on the c-bus
   Serial.print(canId, HEX);
   for (int i = 0; i < len; i++) {
     Serial.print(",");
@@ -112,13 +116,13 @@ void checkIncomingMessages() {
   return;
 #endif
 
+
 #ifdef FILTERED_SCANNER
-  // Targeted (can_id_filter) messages on the c-bus present as matrix on terminal
-  // refressed everey 500ms
+  // Targeted (can_id_filter) messages on the c-bus, csv
   int index = inArray(canId, can_id_filter);
   if(index<99) {
     display[index][0] = canId;
-    for (int i = 1; i <= len; i++) {
+    for (int i = 1; i <= MESSAGE_LEN; i++) {
       display[index][i] = buf[i];
     }
     if (millis() > lastDisplayRefresh + DISPLAY_REFRESH_PERIOD) {
@@ -179,9 +183,12 @@ void checkIncomingMessages() {
 }
 
 void loop() {
-  if (millis() > lastAnnounce + ANNOUNCE_PERIOD_MS) {
-    lastAnnounce = millis();
+  if(millis() > FIRST_ANNOUNCE_AFTER) {
     sendAnnouncements();
+    if (millis() > lastAnnounce + ANNOUNCE_PERIOD_MS) {
+      lastAnnounce = millis();
+      //sendAnnouncements();
+    }
   }
   checkIncomingMessages();
 }
