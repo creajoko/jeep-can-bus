f305d0a96a973b759872bd904c5d5b7548cb5331
creajoko <joakim_korling@hotmail.com>
2021-03-14 11:47:33 +0100

    Fixes so it compiles

--- project/arduino/jeep-boston-amp-controller/jeep-boston-amp-controller.ino
+++ project/arduino/jeep-boston-amp-controller/jeep-boston-amp-controller.ino
@@ -17,13 +17,20 @@ unsigned long lastProfileChange = 0;
 // Messages needed to control Boston amp
 unsigned char msg1[7] = {20, 10, 1, 10, 10, 10, 10};
 unsigned char msg2[7] = {20, 10, 19, 10, 10, 10, 10};
+unsigned char empty[7] = {};
 unsigned char msgPowerOn[8] = {0xFD,0x1B,0x3F,0xFF,0xFF,0xFF,0xFF,0xFF};
 
 const char compileDate[] = __DATE__ " " __TIME__;
 /*
 */
-void sendAnnouncements(unsigned char &profile) {
-  if (profile) {
+unsigned int canId = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+unsigned char newMode = 0;
+unsigned char currProfile = 1;
+
+void sendAnnouncements(unsigned char profile) {
+  if (profile == empty) {
     CAN.sendMsgBuf(CAN_RADIO_SOUND_PROFILE, 0, sizeof(profile), profile);
     delay(CAN_DELAY_AFTER_SEND);
   }
@@ -57,16 +64,11 @@ void setup() {
 /*
 Main loop
 */
-unsigned int canId = 0;
-unsigned char len = 0;
-unsigned char buf[8];
-unsigned char newMode = 0;
-unsigned char currProfile = 1;
 
 void loop() {
     if (millis() > lastAnnounce + ANNOUNCE_PERIOD_MS) {
       lastAnnounce = millis();
-      sendAnnouncements({});
+      sendAnnouncements(empty);
     }
    if (millis() > lastProfileChange + PROFILE_CHANGE_PERIOD_MS) {
         lastProfileChange = millis();
